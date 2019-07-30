a3e905deab6197c8f0d9f3ea4be7f82d1449c59a
Mateusz Gozdek <mgozdekof@gmail.com>
2019-07-31 00:29:48 +0200

    pkg/container/docker: add experimental docker implementation
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/invidian/etcd-ariadnes-thread
 
 go 1.12
+
+require (
+	github.com/Microsoft/go-winio v0.4.13 // indirect
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/docker/docker v1.13.1
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
+	github.com/pkg/errors v0.8.1
+	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80 // indirect
+	golang.org/x/sys v0.0.0-20190726091711-fc99dfbffb4e // indirect
+)
---
+++ pkg/container/docker/docker.go
@@ -0,0 +1,90 @@
+package docker
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/docker/docker/api/types"
+	containertypes "github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/mount"
+	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/client"
+	"github.com/pkg/errors"
+
+	"github.com/invidian/etcd-ariadnes-thread/pkg/container"
+)
+
+// Docker struct represents Docker container runtime
+type Docker struct{}
+
+// Start starts Docker container
+func (d *Docker) Start(config *container.Config) error {
+	fmt.Println("Starting docker container")
+	ctx := context.Background()
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		return fmt.Errorf("Failed to create Docker client: %s", err)
+	}
+	list, err := cli.ContainerList(ctx, types.ContainerListOptions{
+		All: true,
+	})
+	if err != nil {
+		return errors.Wrap(err, "container list")
+	}
+	for _, c := range list {
+		for _, n := range c.Names {
+			fmt.Println("Checkng name:", n)
+			if n == fmt.Sprintf("/%s", config.Name) {
+				fmt.Println("Old container found! Removing...")
+				if err := cli.ContainerRemove(ctx, c.ID, types.ContainerRemoveOptions{}); err != nil {
+					return errors.Wrap(err, "removing old container")
+				}
+			}
+		}
+	}
+
+	reader, err := cli.ImagePull(ctx, config.Image, types.ImagePullOptions{})
+	if err != nil {
+		return errors.Wrap(err, "pulling image")
+	}
+	io.Copy(os.Stdout, reader)
+
+	dockerConfig := containertypes.Config{
+		Image: config.Image,
+		Cmd:   []string{"/bin/sh", "-c", "mount"},
+	}
+	hostConfig := containertypes.HostConfig{
+		Mounts: []mount.Mount{},
+	}
+
+	fmt.Println("Creating container")
+	c, err := cli.ContainerCreate(ctx, &dockerConfig, &hostConfig, &network.NetworkingConfig{}, config.Name)
+	if err != nil {
+		return errors.Wrap(err, "creating container")
+	}
+	ci, err := cli.ContainerInspect(ctx, c.ID)
+	if err != nil {
+		return errors.Wrap(err, "container inspect")
+	}
+	fmt.Println("Created container:", ci.Name)
+	startOptions := types.ContainerStartOptions{}
+
+	if err := cli.ContainerStart(ctx, c.ID, startOptions); err != nil {
+		return errors.Wrap(err, "starting container")
+	}
+	return nil
+}
+
+// Stop stops Docker container
+func (d *Docker) Stop(containerName string) error {
+	return nil
+}
+
+// Status returns container status
+func (d *Docker) Status(containerName string) (*container.Status, error) {
+	return &container.Status{
+		Image: "notimplemented",
+	}, nil
+}
