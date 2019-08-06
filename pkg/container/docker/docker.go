428e0609fb176ae4ea3a9d078895e3a9b6485c88
Mateusz Gozdek <mgozdekof@gmail.com>
2019-08-06 23:38:26 +0200

    pkg/container/docker: update and add tests
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- pkg/container/docker/docker.go
+++ pkg/container/docker/docker.go
@@ -3,6 +3,7 @@ package docker
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/docker/docker/api/types"
 	containertypes "github.com/docker/docker/api/types/container"
@@ -15,73 +16,114 @@ import (
 )
 
 // Docker struct represents Docker container runtime
-type Docker struct{}
+type Docker struct {
+	ctx context.Context
+	cli *client.Client
+}
 
-// Start starts Docker container
-func (d *Docker) Start(config *container.Config) error {
-	fmt.Println("Starting docker container")
-	ctx := context.Background()
+func New() (*Docker, error) {
 	cli, err := client.NewEnvClient()
 	if err != nil {
-		return fmt.Errorf("Failed to create Docker client: %s", err)
+		return nil, errors.Wrap(err, "creating Docker client")
 	}
-	list, err := cli.ContainerList(ctx, types.ContainerListOptions{
-		All: true,
-	})
-	if err != nil {
-		return errors.Wrap(err, "container list")
-	}
-	for _, c := range list {
-		for _, n := range c.Names {
-			fmt.Println("Checkng name:", n)
-			if n == fmt.Sprintf("/%s", config.Name) {
-				fmt.Println("Old container found! Removing...")
-				if err := cli.ContainerRemove(ctx, c.ID, types.ContainerRemoveOptions{}); err != nil {
-					return errors.Wrap(err, "removing old container")
-				}
-			}
-		}
+	return &Docker{
+		ctx: context.Background(),
+		cli: cli,
+	}, nil
+}
+
+func (d *Docker) validate() error {
+	if d.cli == nil || d.ctx == nil {
+		return fmt.Errorf("container runtime is not properly intialized")
 	}
 
-	_, err = cli.ImagePull(ctx, config.Image, types.ImagePullOptions{})
-	if err != nil {
-		return errors.Wrap(err, "pulling image")
+	return nil
+}
+
+// Start starts Docker container
+//
+// This should be generic, so it can be used to start any kind of containers!
+//
+// TODO figure out how to do that on remote machine with SSH
+func (d *Docker) Create(config *container.Config) (string, error) {
+	if err := d.validate(); err != nil {
+		return "", errors.Wrap(err, "creating container failed")
+	}
+	// Pull image to make sure it's available.
+	// TODO make it configurable?
+	if _, err := d.cli.ImagePull(d.ctx, config.Image, types.ImagePullOptions{}); err != nil {
+		return "", errors.Wrap(err, "pulling image")
 	}
 
+	// Just structs required for starting container.
 	dockerConfig := containertypes.Config{
 		Image: config.Image,
-		Cmd:   []string{"/bin/sh", "-c", "mount"},
 	}
 	hostConfig := containertypes.HostConfig{
 		Mounts: []mount.Mount{},
 	}
 
-	fmt.Println("Creating container")
-	c, err := cli.ContainerCreate(ctx, &dockerConfig, &hostConfig, &network.NetworkingConfig{}, config.Name)
+	// Create container
+	c, err := d.cli.ContainerCreate(d.ctx, &dockerConfig, &hostConfig, &network.NetworkingConfig{}, config.Name)
 	if err != nil {
-		return errors.Wrap(err, "creating container")
+		return "", errors.Wrap(err, "creating container")
 	}
-	ci, err := cli.ContainerInspect(ctx, c.ID)
-	if err != nil {
-		return errors.Wrap(err, "container inspect")
-	}
-	fmt.Println("Created container:", ci.Name)
-	startOptions := types.ContainerStartOptions{}
 
-	if err := cli.ContainerStart(ctx, c.ID, startOptions); err != nil {
-		return errors.Wrap(err, "starting container")
+	return c.ID, nil
+}
+
+// Start starts Docker container
+//
+// This should be generic, so it can be used to start any kind of containers!
+//
+// TODO figure out how to do that on remote machine with SSH
+func (d *Docker) Start(ID string) error {
+	if err := d.validate(); err != nil {
+		return errors.Wrap(err, "creating container failed")
 	}
-	return nil
+
+	// And start requested container
+	return d.cli.ContainerStart(d.ctx, ID, types.ContainerStartOptions{})
 }
 
 // Stop stops Docker container
-func (d *Docker) Stop(containerName string) error {
-	return nil
+func (d *Docker) Stop(ID string) error {
+	if err := d.validate(); err != nil {
+		return errors.Wrap(err, "creating container failed")
+	}
+
+	timeout := time.Duration(30) * time.Second
+	return d.cli.ContainerStop(d.ctx, ID, &timeout)
 }
 
 // Status returns container status
-func (d *Docker) Status(containerName string) (*container.Status, error) {
+func (d *Docker) Status(ID string) (*container.Status, error) {
+	if err := d.validate(); err != nil {
+		return nil, errors.Wrap(err, "creating container failed")
+	}
+
+	status, err := d.cli.ContainerInspect(d.ctx, ID)
+	if err != nil {
+		// If container is missing, return no status
+		if client.IsErrNotFound(err) {
+			return nil, nil
+		}
+		return nil, errors.Wrap(err, "inspecting container failed")
+	}
+
 	return &container.Status{
-		Image: "notimplemented",
+		Image:  status.Image,
+		ID:     ID,
+		Name:   status.Name,
+		Status: status.State.Status,
 	}, nil
+}
+
+// Delete removes the container
+func (d *Docker) Delete(ID string) error {
+	if err := d.validate(); err != nil {
+		return errors.Wrap(err, "creating container failed")
+	}
+
+	return d.cli.ContainerRemove(d.ctx, ID, types.ContainerRemoveOptions{})
 }
---
+++ pkg/container/docker/docker_integration_test.go
@@ -0,0 +1,117 @@
+// +build integration
+
+package docker
+
+import (
+	"testing"
+
+	"github.com/invidian/etcd-ariadnes-thread/pkg/container"
+)
+
+// Create
+func TestContainerCreate(t *testing.T) {
+	d, err := New()
+	if err != nil {
+		t.Errorf("Creating new docker runtime should succeed, got: %s", err)
+	}
+	c := &container.Config{
+		Image: "gcr.io/etcd-development/etcd:v3.3.13",
+	}
+
+	if _, err = d.Create(c); err != nil {
+		t.Errorf("Creating container should succeed, got: %s", err)
+	}
+}
+
+func TestContainerCreateDelete(t *testing.T) {
+	d, err := New()
+	if err != nil {
+		t.Errorf("Creating new docker runtime should succeed, got: %s", err)
+	}
+	c := &container.Config{
+		Image: "gcr.io/etcd-development/etcd:v3.3.13",
+	}
+	id, err := d.Create(c)
+	if err != nil {
+		t.Errorf("Creating container should succeed, got: %s", err)
+	}
+
+	if err := d.Delete(id); err != nil {
+		t.Errorf("Removing container should succeed, got: %s", err)
+	}
+}
+
+// Start()
+func TestContainerStart(t *testing.T) {
+	d, err := New()
+	if err != nil {
+		t.Errorf("Creating new docker runtime should succeed, got: %s", err)
+	}
+	c := &container.Config{
+		Image: "gcr.io/etcd-development/etcd:v3.3.13",
+	}
+	id, err := d.Create(c)
+	if err != nil {
+		t.Errorf("Creating container should succeed, got: %s", err)
+	}
+
+	if err := d.Start(id); err != nil {
+		t.Errorf("Starting container should work, got: %s", err)
+	}
+}
+
+// Stop()
+func TestContainerStop(t *testing.T) {
+	d, err := New()
+	if err != nil {
+		t.Errorf("Creating new docker runtime should succeed, got: %s", err)
+	}
+	c := &container.Config{
+		Image: "gcr.io/etcd-development/etcd:v3.3.13",
+	}
+	id, err := d.Create(c)
+	if err != nil {
+		t.Errorf("Creating container should succeed, got: %s", err)
+	}
+	if err := d.Start(id); err != nil {
+		t.Errorf("Starting container should work, got: %s", err)
+	}
+
+	if err := d.Stop(id); err != nil {
+		t.Errorf("Stopping container should work, got: %s", err)
+	}
+}
+
+// Status()
+func TestContainerStatus(t *testing.T) {
+	d, err := New()
+	if err != nil {
+		t.Errorf("Creating new docker runtime should succeed, got: %s", err)
+	}
+	c := &container.Config{
+		Image: "gcr.io/etcd-development/etcd:v3.3.13",
+	}
+	id, err := d.Create(c)
+	if err != nil {
+		t.Errorf("Creating container should succeed, got: %s", err)
+	}
+
+	if _, err = d.Status(id); err != nil {
+		t.Errorf("Getting container status should work, got: %s", err)
+	}
+}
+
+func TestContainerStatusNonExistent(t *testing.T) {
+	d, err := New()
+	if err != nil {
+		t.Errorf("Creating new docker runtime should succeed, got: %s", err)
+	}
+
+	status, err := d.Status("nonexistent")
+	if err != nil {
+		t.Errorf("Getting non-existent container status shouldn't return error, got: %s", err)
+	}
+	if status != nil {
+		t.Errorf("Getting non-existent container status shouldn't return any status")
+	}
+}
