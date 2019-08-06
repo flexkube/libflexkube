94e6f28bf90577e49176e487caabf35a48adaa03
Mateusz Gozdek <mgozdekof@gmail.com>
2019-08-06 23:37:53 +0200

    pkg/container: extend interface with new required methods
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- pkg/container/container.go
+++ pkg/container/container.go
@@ -2,9 +2,16 @@ package container
 
 // Container interface describes universal way of starting containers
 type Container interface {
-	Start(*Config) error
-	Stop(string) error
+	// Create creates container and returns it's unique identifier
+	Create(*Config) (string, error)
+	// Delete removes the container
+	Delete(string) error
+	// Start starts created container
+	Start(string) error
+	// Status returns status of the container
 	Status(string) (*Status, error)
+	// Stop takes unique identifier as a parameter and stops the container
+	Stop(string) error
 }
 
 // Config describes how container should be started
@@ -15,5 +22,8 @@ type Config struct {
 
 // Status describes what informations are returned about container
 type Status struct {
-	Image string
+	Image  string `json:"image"`
+	ID     string `json:"id"`
+	Name   string `json:"name"`
+	Status string `json:"status"`
 }
