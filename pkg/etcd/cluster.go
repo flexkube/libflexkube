ab7a05678dd45aa8b0ad13430e27b8b60c29d72b
Mateusz Gozdek <mgozdekof@gmail.com>
2019-09-29 00:39:28 +0200

    pkg/etcd: add initial implementation
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- pkg/etcd/cluster.go
+++ pkg/etcd/cluster.go
@@ -1,4 +1,194 @@
 package etcd
 
-// Cluster represents etcd cluster
-type Cluster struct{}
+import (
+	"fmt"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+
+	"github.com/invidian/etcd-ariadnes-thread/pkg/container"
+	"github.com/invidian/etcd-ariadnes-thread/pkg/host"
+	"github.com/invidian/etcd-ariadnes-thread/pkg/host/transport/direct"
+	"github.com/invidian/etcd-ariadnes-thread/pkg/host/transport/ssh"
+)
+
+// Cluster represents etcd cluster configuration and state from the user
+type Cluster struct {
+	// User-configurable fields
+	Image             string            `json:"image,omitempty" yaml:"image,omitempty"`
+	SSH               *ssh.SSHConfig    `json:"ssh,omitempty" yaml:"ssh,omitempty"`
+	PeerCACertificate string            `json:"peerCACertificate,omitempty" yaml:"peerCACertificate,omitempty"`
+	Members           map[string]Member `json:"members,omitempty" yaml:"members,omitempty"`
+
+	// Serializable fields
+	State container.ContainersState `json:"state:omitempty" yaml:"state,omitempty"`
+}
+
+// cluster is executable version of Cluster, with validated fields and calculated containers
+type cluster struct {
+	image             string
+	ssh               *ssh.SSHConfig
+	peerCACertificate string
+	containers        container.Containers
+}
+
+func (c *Cluster) New() (*cluster, error) {
+	if err := c.Validate(); err != nil {
+		return nil, fmt.Errorf("failed to validate cluster configuration: %w", err)
+	}
+
+	cluster := &cluster{
+		image:             c.Image,
+		ssh:               c.SSH,
+		peerCACertificate: c.PeerCACertificate,
+		containers: container.Containers{
+			PreviousState: c.State,
+			DesiredState:  make(container.ContainersState),
+		},
+	}
+
+	initialClusterArr := []string{}
+	peerCertAllowedCNArr := []string{}
+
+	for n, m := range c.Members {
+		initialClusterArr = append(initialClusterArr, fmt.Sprintf("%s=https://%s:2380", n, m.PeerAddress))
+		peerCertAllowedCNArr = append(peerCertAllowedCNArr, fmt.Sprintf("etcd-%s", n))
+	}
+
+	initialCluster := strings.Join(initialClusterArr, ",")
+	peerCertAllowedCN := strings.Join(peerCertAllowedCNArr, ",")
+
+	for n, m := range c.Members {
+		if m.Name == "" {
+			m.Name = n
+		}
+		if m.Image == "" && c.Image != "" {
+			m.Image = c.Image
+		}
+		if m.InitialCluster == "" {
+			m.InitialCluster = initialCluster
+		}
+		if m.PeerCertAllowedCN == "" {
+			m.PeerCertAllowedCN = peerCertAllowedCN
+		}
+
+		// TODO find better way to handle defaults!!!
+		if m.Host == nil || (m.Host.DirectConfig == nil && m.Host.SSHConfig == nil) {
+			m.Host = &host.Host{
+				DirectConfig: &direct.DirectConfig{},
+			}
+		}
+		if m.Host != nil && m.Host.SSHConfig != nil && m.Host.SSHConfig.PrivateKey == "" && c.SSH != nil && c.SSH.PrivateKey != "" {
+			m.Host.SSHConfig.PrivateKey = c.SSH.PrivateKey
+		}
+
+		if m.Host != nil && m.Host.SSHConfig != nil && m.Host.SSHConfig.User == "" && c.SSH != nil && c.SSH.User != "" {
+			m.Host.SSHConfig.User = c.SSH.User
+		}
+		if m.Host != nil && m.Host.SSHConfig != nil && m.Host.SSHConfig.User == "" {
+			m.Host.SSHConfig.User = "root"
+		}
+
+		if m.Host != nil && m.Host.SSHConfig != nil && m.Host.SSHConfig.ConnectionTimeout == "" && c.SSH != nil && c.SSH.ConnectionTimeout != "" {
+			m.Host.SSHConfig.ConnectionTimeout = c.SSH.ConnectionTimeout
+		}
+		if m.Host != nil && m.Host.SSHConfig != nil && m.Host.SSHConfig.ConnectionTimeout == "" {
+			m.Host.SSHConfig.ConnectionTimeout = "30s"
+		}
+
+		if m.Host != nil && m.Host.SSHConfig != nil && m.Host.SSHConfig.Port == 0 && c.SSH != nil && c.SSH.Port != 0 {
+			m.Host.SSHConfig.Port = c.SSH.Port
+		}
+		if m.Host != nil && m.Host.SSHConfig != nil && m.Host.SSHConfig.Port == 0 {
+			m.Host.SSHConfig.Port = 22
+		}
+
+		if m.PeerCACertificate == "" && c.PeerCACertificate != "" {
+			m.PeerCACertificate = c.PeerCACertificate
+		}
+
+		member, err := m.New()
+		if err != nil {
+			return nil, fmt.Errorf("that was unexpected: %w", err)
+		}
+		cluster.containers.DesiredState[n] = member.ToHostConfiguredContainer()
+	}
+
+	return cluster, nil
+}
+
+func (c *Cluster) Validate() error {
+	if len(c.Members) < 0 && c.State == nil {
+		return fmt.Errorf("either members or previous state needs to be defined")
+	}
+
+	for n, m := range c.Members {
+		if m.Name == "" {
+			m.Name = n
+		}
+		if m.Host == nil || m.Host.DirectConfig == nil || m.Host.SSHConfig == nil {
+			m.Host = &host.Host{
+				DirectConfig: &direct.DirectConfig{},
+			}
+		}
+		if err := m.Validate(); err != nil {
+			return fmt.Errorf("failed to validate member '%s': %w", n, err)
+		}
+	}
+
+	return nil
+}
+
+// FromYaml allows to restore cluster state from YAML.
+func FromYaml(c []byte) (*cluster, error) {
+	cluster := &Cluster{}
+	if err := yaml.Unmarshal(c, &cluster); err != nil {
+		return nil, fmt.Errorf("failed to parse input yaml: %w", err)
+	}
+	cl, err := cluster.New()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create cluster object: %w", err)
+	}
+	return cl, nil
+}
+
+// StateToYaml allows to dump cluster state to YAML, so it can be restored later.
+func (c *cluster) StateToYaml() ([]byte, error) {
+	return yaml.Marshal(Cluster{State: c.containers.PreviousState})
+}
+
+func (c *cluster) CheckCurrentState() error {
+	containers, err := c.containers.New()
+	if err != nil {
+		return err
+	}
+	if err := containers.CheckCurrentState(); err != nil {
+		return err
+	}
+	c.containers = *containers.ToExported()
+	return nil
+}
+
+func (c *cluster) Deploy() error {
+	containers, err := c.containers.New()
+	if err != nil {
+		return err
+	}
+	// TODO Deploy shouldn't refresh the state. However, due to how we handle exported/unexported
+	// structs to enforce validation of objects, we lose current state, as we want it to be computed.
+	// On the other hand, maybe it's a good thing to call it once we execute. This way we could compare
+	// the plan user agreed to execute with plan calculated right before the execution and fail early if they
+	// differ.
+	// This is similar to what terraform is doing and may cause planning to run several times, so it may require
+	// some optimization.
+	// Alternatively we can have serializable plan and a knob in execute command to control wheather we should
+	// make additional validation or not.
+	if err := containers.CheckCurrentState(); err != nil {
+		return err
+	}
+	if err := containers.Execute(); err != nil {
+		return err
+	}
+	c.containers = *containers.ToExported()
+	return nil
+}
--- pkg/etcd/member.go
+++ pkg/etcd/member.go
@@ -1,4 +1,161 @@
 package etcd
 
+import (
+	"fmt"
+
+	"github.com/invidian/etcd-ariadnes-thread/pkg/container"
+	"github.com/invidian/etcd-ariadnes-thread/pkg/container/runtime/docker"
+	"github.com/invidian/etcd-ariadnes-thread/pkg/container/types"
+	"github.com/invidian/etcd-ariadnes-thread/pkg/defaults"
+	"github.com/invidian/etcd-ariadnes-thread/pkg/host"
+)
+
 // Member represents single etcd member
-type Member struct{}
+type Member struct {
+	Name              string     `json:"name,omitempty" yaml:"name,omitempty"`
+	Image             string     `json:"image,omitempty" yaml:"image,omitempty"`
+	Host              *host.Host `json:"host,omitempty" yaml:"host,omitempty"`
+	PeerCACertificate string     `json:"peerCACertificate,omitempty" yaml:"peerCACertificate,omitempty"`
+	PeerCertificate   string     `json:"peerCertificate,omitempty" yaml:"peerCertificate,omitempty"`
+	PeerKey           string     `json:"peerKey,omitempty" yaml:"peerKey,omitempty"`
+	PeerAddress       string     `json:"peerAddress,omitempty" yaml:"peerAddress,omitempty"`
+	InitialCluster    string     `json:"initialCluster,omitempty" yaml:"initialCluster,omitempty"`
+	PeerCertAllowedCN string     `json:"peerCertAllowedCN,omitempty" yaml:"peerCertAllowedCN,omitempty"`
+}
+
+// member is a validated, executable version of Member
+type member struct {
+	name              string
+	image             string
+	host              host.Host
+	peerCACertificate string
+	peerCertificate   string
+	peerKey           string
+	peerAddress       string
+	initialCluster    string
+	peerCertAllowedCN string
+}
+
+func (m *member) ToHostConfiguredContainer() *container.HostConfiguredContainer {
+	configFiles := make(map[string]string)
+	if m.peerCACertificate != "" && m.peerCertificate != "" && m.peerCertificate != "" {
+		configFiles["/etc/kubernetes/pki/etcd/ca.crt"] = m.peerCACertificate
+		configFiles["/etc/kubernetes/pki/etcd/peer.crt"] = m.peerCertificate
+		configFiles["/etc/kubernetes/pki/etcd/peer.key"] = m.peerKey
+	}
+
+	c := container.Container{
+		// TODO this is weird. This sets docker as default runtime config
+		Runtime: container.RuntimeConfig{
+			Docker: &docker.ClientConfig{},
+		},
+		Config: types.ContainerConfig{
+			Name:       m.name,
+			Image:      m.image,
+			Entrypoint: []string{"/usr/local/bin/etcd"},
+			Ports: []types.PortMap{
+				types.PortMap{
+					IP:       m.peerAddress,
+					Protocol: "tcp",
+					Port:     2379,
+				},
+				types.PortMap{
+					IP:       m.peerAddress,
+					Protocol: "tcp",
+					Port:     2380,
+				},
+			},
+			Mounts: []types.Mount{
+				types.Mount{
+					Source: fmt.Sprintf("/tmp/etcd-cluster-2/%s.etcd/", m.name),
+					Target: fmt.Sprintf("/%s.etcd", m.name),
+				},
+				types.Mount{
+					Source: "/etc/kubernetes/pki/etcd/",
+					Target: "/etc/kubernetes/pki/etcd",
+				},
+			},
+			Args: []string{
+				//TODO Add descriptions explaining why we need each line.
+				// Default value 'capnslog' for logger is deprecated and prints warning now.
+				"--logger=zap",
+				// Since we are in container, listen on all interfaces
+				"--listen-client-urls=http://0.0.0.0:2379",
+				"--listen-peer-urls=https://0.0.0.0:2380",
+				fmt.Sprintf("--advertise-client-urls=http://%s:2379", m.peerAddress),
+				fmt.Sprintf("--initial-advertise-peer-urls=https://%s:2380", m.peerAddress),
+				fmt.Sprintf("--initial-cluster=%s", m.initialCluster),
+				fmt.Sprintf("--name=%s", m.name),
+				"--initial-cluster-token=etcd-cluster-2",
+				"--peer-trusted-ca-file=/etc/kubernetes/pki/etcd/ca.crt",
+				"--peer-cert-file=/etc/kubernetes/pki/etcd/peer.crt",
+				"--peer-key-file=/etc/kubernetes/pki/etcd/peer.key",
+				"--peer-client-cert-auth",
+				fmt.Sprintf("--data-dir=/%s.etcd", m.name),
+				// To get rid of warning with default configuration
+				"--auth-token=jwt,pub-key=/etc/kubernetes/pki/etcd/peer.crt,priv-key=/etc/kubernetes/pki/etcd/peer.key,sign-method=RS512,ttl=10m",
+				// This is set by typhoon, seems like extra safety knob
+				"--strict-reconfig-check",
+				// TODO enable metrics
+			},
+		},
+	}
+
+	return &container.HostConfiguredContainer{
+		Host:        m.host,
+		ConfigFiles: configFiles,
+		Container:   c,
+	}
+}
+
+func (m *member) ToExistingClusterMember() *container.HostConfiguredContainer {
+	return nil
+}
+
+func (m *member) ToNewClusterMember() *container.HostConfiguredContainer {
+	return nil
+}
+
+func (m *Member) New() (*member, error) {
+	if err := m.Validate(); err != nil {
+		return nil, fmt.Errorf("failed to validate member configuration: %w", err)
+	}
+
+	nm := &member{
+		name:              m.Name,
+		image:             m.Image,
+		host:              *m.Host,
+		peerCACertificate: m.PeerCACertificate,
+		peerCertificate:   m.PeerCertificate,
+		peerKey:           m.PeerKey,
+		peerAddress:       m.PeerAddress,
+		initialCluster:    m.InitialCluster,
+		peerCertAllowedCN: m.PeerCertAllowedCN,
+	}
+
+	if nm.image == "" {
+		nm.image = defaults.EtcdImage
+	}
+
+	return nm, nil
+}
+
+// TODO add validation of certificates if specified
+func (m *Member) Validate() error {
+	// TODO require peer address for now. Later we could figure out
+	// how to use CNI for setting it using env variables or something
+	if m.PeerAddress == "" {
+		return fmt.Errorf("peer address must be set")
+	}
+
+	// TODO can we auto-generate it?
+	if m.Name == "" {
+		return fmt.Errorf("member name must be set")
+	}
+
+	if m.Host == nil {
+		return fmt.Errorf("host configuration must be defined")
+	}
+
+	return nil
+}
