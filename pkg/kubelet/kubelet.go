4906d3b7fe5e69fc6388f37be9a1fb0c4470c340
Mateusz Gozdek <mgozdekof@gmail.com>
2019-10-14 20:20:15 +0200

    pkg/kubelet: add initial implementation
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -1,4 +1,128 @@
 package kubelet
 
+import (
+	"fmt"
+
+	"github.com/invidian/flexkube/pkg/container"
+	"github.com/invidian/flexkube/pkg/container/runtime/docker"
+	"github.com/invidian/flexkube/pkg/container/types"
+	"github.com/invidian/flexkube/pkg/defaults"
+	"github.com/invidian/flexkube/pkg/host"
+)
+
 // Instance represents single kubelet instance
-type Instance struct{}
+type Kubelet struct {
+	Address             string     `json:"address,omitempty" yaml:"address,omitempty"`
+	Image               string     `json:"image,omitempty" yaml:"image,omitempty"`
+	Host                *host.Host `json:"host,omitempty" yaml:"host,omitempty"`
+	BootstrapKubeconfig string     `json:"bootstrapKubeconfig,omitempty" yaml:"bootstrapKubeconfig,omitempty"`
+}
+
+// kubelet is a validated, executable version of Kubelet
+type kubelet struct {
+	address             string
+	image               string
+	host                *host.Host
+	bootstrapKubeconfig string
+}
+
+func (k *Kubelet) New() (*kubelet, error) {
+	if err := k.Validate(); err != nil {
+		return nil, fmt.Errorf("failed to validate kubelet configuration: %w", err)
+	}
+
+	nk := &kubelet{
+		image:               k.Image,
+		address:             k.Address,
+		host:                k.Host,
+		bootstrapKubeconfig: k.BootstrapKubeconfig,
+	}
+
+	if nk.image == "" {
+		nk.image = defaults.KubernetesImage
+	}
+
+	return nk, nil
+}
+
+func (k *Kubelet) Validate() error {
+	// TODO better validation should be done here
+	if k.BootstrapKubeconfig == "" {
+		return fmt.Errorf("bootstrapKubeconfig can't be empty")
+	}
+
+	return nil
+}
+
+func (k *kubelet) ToHostConfiguredContainer() *container.HostConfiguredContainer {
+	configFiles := make(map[string]string)
+	// TODO maybe we store that as a struct, and we marshal here to YAML?
+	configFiles["/etc/kubernetes/kubelet/kubelet.yaml"] = `apiVersion: kubelet.config.k8s.io/v1beta1
+kind: KubeletConfiguration
+staticPodPath: /etc/kubernetes/manifests
+rotateCertificates: true
+cgroupsPerQOS: false
+enforceNodeAllocatable: []
+cgroupDriver: systemd
+`
+	configFiles["/etc/kubernetes/kubelet/bootstrap-kubeconfig"] = k.bootstrapKubeconfig
+
+	c := container.Container{
+		// TODO this is weird. This sets docker as default runtime config
+		Runtime: container.RuntimeConfig{
+			Docker: &docker.ClientConfig{},
+		},
+		Config: types.ContainerConfig{
+			// TODO make it configurable?
+			Name:  "kubelet",
+			Image: k.image,
+			// TODO perhaps entrypoint should be a string, not array of strings? we use args for arguments anyway
+			Entrypoint: []string{"/kubelet"},
+			Ports: []types.PortMap{
+				types.PortMap{
+					IP:       k.address,
+					Protocol: "tcp",
+					Port:     10250,
+				},
+			},
+			Privileged: true,
+			Mounts: []types.Mount{
+				types.Mount{
+					Source: "/etc/kubernetes/kubelet/",
+					Target: "/etc/kubernetes/",
+				},
+				types.Mount{
+					Source: "/etc/kubernetes/kubelet/manifests/",
+					Target: "/etc/kubernetes/manifests",
+				},
+				types.Mount{
+					Source: "/run/docker.sock",
+					Target: "/var/run/docker.sock",
+				},
+				types.Mount{
+					Source:      "/var/lib/docker/",
+					Target:      "/var/lib/docker",
+					Propagation: "rslave",
+				},
+				types.Mount{
+					Source:      "/var/lib/kubelet/",
+					Target:      "/var/lib/kubelet",
+					Propagation: "shared",
+				},
+			},
+			Args: []string{
+				"--config=/etc/kubernetes/kubelet.yaml",
+				"--kubeconfig=/etc/kubernetes/kubeconfig",
+				"--bootstrap-kubeconfig=/etc/kubernetes/bootstrap-kubeconfig",
+				"--v=2",
+				"--network-plugin=kubenet",
+			},
+		},
+	}
+
+	return &container.HostConfiguredContainer{
+		Host:        *k.host,
+		ConfigFiles: configFiles,
+		Container:   c,
+	}
+}
--- pkg/kubelet/pool.go
+++ pkg/kubelet/pool.go
@@ -1,6 +1,153 @@
 package kubelet
 
+import (
+	"fmt"
+	"strconv"
+
+	"gopkg.in/yaml.v3"
+
+	"github.com/invidian/flexkube/pkg/container"
+	"github.com/invidian/flexkube/pkg/host"
+	"github.com/invidian/flexkube/pkg/host/transport/direct"
+	"github.com/invidian/flexkube/pkg/host/transport/ssh"
+)
+
 // Pool represents group of kubelet instances and their configuration
 type Pool struct {
-	Kubelets []Instance
+	// User-configurable fields
+	Image               string         `json:"image,omitempty" yaml:"image,omitempty"`
+	SSH                 *ssh.SSHConfig `json:"ssh,omitempty" yaml:"ssh,omitempty"`
+	BootstrapKubeconfig string         `json:"bootstrapKubeconfig,omitempty" yaml:"bootstrapKubeconfig,omitempty"`
+	Kubelets            []Kubelet      `josn:"kubelets,omitempty" yaml:"kubelets,omitempty"`
+
+	// Serializable fields
+	State container.ContainersState `json:"state:omitempty" yaml:"state,omitempty"`
+}
+
+type pool struct {
+	image      string
+	ssh        *ssh.SSHConfig
+	containers container.Containers
+}
+
+func (p *Pool) New() (*pool, error) {
+	if err := p.Validate(); err != nil {
+		return nil, fmt.Errorf("failed to validate pool configuration: %w", err)
+	}
+
+	pool := &pool{
+		image: p.Image,
+		ssh:   p.SSH,
+		containers: container.Containers{
+			PreviousState: p.State,
+			DesiredState:  make(container.ContainersState),
+		},
+	}
+
+	for i, k := range p.Kubelets {
+		if k.Image == "" && p.Image != "" {
+			k.Image = p.Image
+		}
+		if k.BootstrapKubeconfig == "" && p.BootstrapKubeconfig != "" {
+			k.BootstrapKubeconfig = p.BootstrapKubeconfig
+		}
+
+		// TODO find better way to handle defaults!!!
+		if k.Host == nil || (k.Host.DirectConfig == nil && k.Host.SSHConfig == nil) {
+			k.Host = &host.Host{
+				DirectConfig: &direct.DirectConfig{},
+			}
+		}
+		if k.Host != nil && k.Host.SSHConfig != nil && k.Host.SSHConfig.PrivateKey == "" && p.SSH != nil && p.SSH.PrivateKey != "" {
+			k.Host.SSHConfig.PrivateKey = p.SSH.PrivateKey
+		}
+
+		if k.Host != nil && k.Host.SSHConfig != nil && k.Host.SSHConfig.User == "" && p.SSH != nil && p.SSH.User != "" {
+			k.Host.SSHConfig.User = p.SSH.User
+		}
+		if k.Host != nil && k.Host.SSHConfig != nil && k.Host.SSHConfig.User == "" {
+			k.Host.SSHConfig.User = "root"
+		}
+
+		if k.Host != nil && k.Host.SSHConfig != nil && k.Host.SSHConfig.ConnectionTimeout == "" && p.SSH != nil && p.SSH.ConnectionTimeout != "" {
+			k.Host.SSHConfig.ConnectionTimeout = p.SSH.ConnectionTimeout
+		}
+		if k.Host != nil && k.Host.SSHConfig != nil && k.Host.SSHConfig.ConnectionTimeout == "" {
+			k.Host.SSHConfig.ConnectionTimeout = "30s"
+		}
+
+		if k.Host != nil && k.Host.SSHConfig != nil && k.Host.SSHConfig.Port == 0 && p.SSH != nil && p.SSH.Port != 0 {
+			k.Host.SSHConfig.Port = p.SSH.Port
+		}
+		if k.Host != nil && k.Host.SSHConfig != nil && k.Host.SSHConfig.Port == 0 {
+			k.Host.SSHConfig.Port = 22
+		}
+
+		kubelet, err := k.New()
+		if err != nil {
+			return nil, fmt.Errorf("that was unexpected: %w", err)
+		}
+		pool.containers.DesiredState[strconv.Itoa(i)] = kubelet.ToHostConfiguredContainer()
+	}
+
+	return pool, nil
+}
+
+// TODO add validation
+func (p *Pool) Validate() error {
+	return nil
+}
+
+// FromYaml allows to restore cluster state from YAML.
+func FromYaml(c []byte) (*pool, error) {
+	pool := &Pool{}
+	if err := yaml.Unmarshal(c, &pool); err != nil {
+		return nil, fmt.Errorf("failed to parse input yaml: %w", err)
+	}
+	p, err := pool.New()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create cluster object: %w", err)
+	}
+	return p, nil
+}
+
+// StateToYaml allows to dump cluster state to YAML, so it can be restored later.
+func (p *pool) StateToYaml() ([]byte, error) {
+	return yaml.Marshal(Pool{State: p.containers.PreviousState})
+}
+
+func (p *pool) CheckCurrentState() error {
+	containers, err := p.containers.New()
+	if err != nil {
+		return err
+	}
+	if err := containers.CheckCurrentState(); err != nil {
+		return err
+	}
+	p.containers = *containers.ToExported()
+	return nil
+}
+
+func (p *pool) Deploy() error {
+	containers, err := p.containers.New()
+	if err != nil {
+		return err
+	}
+	// TODO Deploy shouldn't refresh the state. However, due to how we handle exported/unexported
+	// structs to enforce validation of objects, we lose current state, as we want it to be computed.
+	// On the other hand, maybe it's a good thing to call it once we execute. This way we could compare
+	// the plan user agreed to execute with plan calculated right before the execution and fail early if they
+	// differ.
+	// This is similar to what terraform is doing and may cause planning to run several times, so it may require
+	// some optimization.
+	// Alternatively we can have serializable plan and a knob in execute command to control wheather we should
+	// make additional validation or not.
+	if err := containers.CheckCurrentState(); err != nil {
+		return err
+	}
+	if err := containers.Execute(); err != nil {
+		return err
+	}
+	p.containers = *containers.ToExported()
+	return nil
 }
