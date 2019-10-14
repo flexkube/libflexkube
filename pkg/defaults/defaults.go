77b3bbaa42676c2b07e5216a77d7cb95a6271b43
Mateusz Gozdek <mgozdekof@gmail.com>
2019-10-14 20:18:03 +0200

    pkg/defaults: add default kubernetes image
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- pkg/defaults/defaults.go
+++ pkg/defaults/defaults.go
@@ -1,4 +1,5 @@
 package defaults
 
-const EtcdImage = "gcr.io/etcd-development/etcd:v3.4.1"
+const EtcdImage = "gcr.io/etcd-development/etcd:v3.4.2"
+const KubernetesImage = "k8s.gcr.io/hyperkube:v1.16.1"
 const DockerAPIVersion = "v1.40"
