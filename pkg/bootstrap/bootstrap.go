9449ad2a0802c8cc2e00042d5cb75902824e000f
Mateusz Gozdek <mgozdekof@gmail.com>
2019-10-26 09:30:59 +0200

    pkg/bootstrap: add a bit more comments
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- pkg/bootstrap/bootstrap.go
+++ pkg/bootstrap/bootstrap.go
@@ -3,3 +3,7 @@ package bootstrap
 // bootstrap start static control plane on the kubelet using 'controlplane' package,
 // deploys kubernetes component to it and shuts down static control plane, when self-hosted
 // is up
+//
+// It glues following components:
+// - controlplane
+// - components/kubernetes
