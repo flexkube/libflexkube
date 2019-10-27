4ba1daec691de2651d92ac5b0010dde80fae9c7d
Mateusz Gozdek <mgozdekof@gmail.com>
2019-10-27 15:16:47 +0100

    pkg/defaults: bump default hyperkube version to 1.16.2
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- pkg/defaults/defaults.go
+++ pkg/defaults/defaults.go
@@ -2,6 +2,6 @@ package defaults
 
 // use etcd 3.3.17, because 3.4.x is not yet supported
 const EtcdImage = "gcr.io/etcd-development/etcd:v3.3.17"
-const KubernetesImage = "k8s.gcr.io/hyperkube:v1.16.1"
+const KubernetesImage = "k8s.gcr.io/hyperkube:v1.16.2"
 const HAProxyImage = "haproxy:2.0.7-alpine"
 const DockerAPIVersion = "v1.40"
