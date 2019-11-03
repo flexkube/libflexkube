7842f84ea79f508dff1b5d5e9df9c35794c25227
Mateusz Gozdek <mgozdekof@gmail.com>
2019-11-03 23:10:51 +0100

    resource_helm_release: use pkg/helm/release
    This commit removes all hardcoded helm logic and switches code to use
    library structures from pkg/helm/release.
    
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- cmd/terraform-provider-flexkube/flexkube/resource_helm_release.go
+++ cmd/terraform-provider-flexkube/flexkube/resource_helm_release.go
@@ -1,26 +1,8 @@
 package flexkube
 
 import (
-	"fmt"
-
 	"github.com/hashicorp/terraform/helper/schema"
-
-	"helm.sh/helm/v3/pkg/action"
-	"helm.sh/helm/v3/pkg/chart/loader"
-	"helm.sh/helm/v3/pkg/cli"
-	"helm.sh/helm/v3/pkg/kube"
-	"helm.sh/helm/v3/pkg/storage"
-	"helm.sh/helm/v3/pkg/storage/driver"
-	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
-	"k8s.io/apimachinery/pkg/api/meta"
-	"k8s.io/client-go/discovery"
-	"k8s.io/client-go/discovery/cached/memory"
-	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/restmapper"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/kubectl/pkg/cmd/util"
-	"sigs.k8s.io/yaml"
+	"github.com/invidian/libflexkube/pkg/helm"
 )
 
 func resourceHelmRelease() *schema.Resource {
@@ -55,135 +37,80 @@ func resourceHelmRelease() *schema.Resource {
 }
 
 func resourceHelmReleaseCreate(d *schema.ResourceData, m interface{}) error {
-	namespace := d.Get("namespace").(string)
-
-	// Initialize kubernetes and helm CLI clients
-	actionConfig := &action.Configuration{}
-	settings := cli.New()
-
-	// Inlining helm.sh/helm/v3/pkg/kube.New() to be able to override the config
-	if err := v1beta1.AddToScheme(scheme.Scheme); err != nil {
-		// This should never happen.
-		panic(err)
+	r := helm.Release{
+		Kubeconfig: d.Get("kubeconfig").(string),
+		Namespace:  d.Get("namespace").(string),
+		Name:       d.Get("name").(string),
+		Chart:      d.Get("chart").(string),
+		Values:     d.Get("values").(string),
+		Version:    ">0.0.0-0",
 	}
 
-	getter, err := GetGetter([]byte(d.Get("kubeconfig").(string)))
+	release, err := r.New()
 	if err != nil {
-		panic(err)
+		return err
 	}
 
-	kc := &kube.Client{
-		Factory: util.NewFactory(getter),
-		Log:     func(_ string, _ ...interface{}) {},
+	if err := release.InstallOrUpgrade(); err != nil {
+		return err
 	}
 
-	clientset, err := kc.Factory.KubernetesClientSet()
-	if err != nil {
-		panic(err)
-	}
-
-	actionConfig.RESTClientGetter = getter
-	actionConfig.KubeClient = kc
-	actionConfig.Releases = storage.Init(driver.NewSecrets(clientset.CoreV1().Secrets(namespace)))
-
-	// Initialize install action client
-	client := action.NewInstall(actionConfig)
-
-	// Set version of the chart
-	client.Version = ">0.0.0-0"
-
-	// Set release name
-	client.ReleaseName = d.Get("name").(string)
-
-	// Set namespace
-	client.Namespace = namespace
-
-	// Locate chart to install, here we install chart from local folder
-	cp, err := client.ChartPathOptions.LocateChart(d.Get("chart").(string), settings)
-	if err != nil {
-		return fmt.Errorf("locating chart failed: %w", err)
-	}
-
-	values := map[string]interface{}{}
-	if err := yaml.Unmarshal([]byte(d.Get("values").(string)), &values); err != nil {
-		return fmt.Errorf("failed to parse values: %w", err)
-	}
-
-	// Load the chart
-	chartRequested, err := loader.Load(cp)
-	if err != nil {
-		return fmt.Errorf("loading chart failed: %w", err)
-	}
-
-	// Install a release
-	if _, err = client.Run(chartRequested, values); err != nil {
-		return fmt.Errorf("installing a release failed: %w", err)
-	}
-
-	result := sha256sum([]byte(d.Get("chart").(string) + d.Get("name").(string) + namespace + d.Get("kubeconfig").(string)))
+	result := sha256sum([]byte(d.Get("chart").(string) + d.Get("name").(string) + d.Get("namespace").(string) + d.Get("kubeconfig").(string)))
 	d.SetId(result)
 
 	return nil
 }
 
 func resourceHelmReleaseRead(d *schema.ResourceData, m interface{}) error {
+	r := helm.Release{
+		Kubeconfig: d.Get("kubeconfig").(string),
+		Namespace:  d.Get("namespace").(string),
+		Name:       d.Get("name").(string),
+		Chart:      d.Get("chart").(string),
+		Values:     d.Get("values").(string),
+		Version:    ">0.0.0-0",
+	}
+
+	release, err := r.New()
+	if err != nil {
+		return err
+	}
+
+	e, err := release.Exists()
+	if err != nil {
+		return err
+	}
+
+	if e {
+		result := sha256sum([]byte(d.Get("chart").(string) + d.Get("name").(string) + d.Get("namespace").(string) + d.Get("kubeconfig").(string)))
+		d.SetId(result)
+	} else {
+		d.SetId("")
+	}
+
 	return nil
 }
 
 func resourceHelmReleaseDelete(d *schema.ResourceData, m interface{}) error {
+	r := helm.Release{
+		Kubeconfig: d.Get("kubeconfig").(string),
+		Namespace:  d.Get("namespace").(string),
+		Name:       d.Get("name").(string),
+		Chart:      d.Get("chart").(string),
+		Values:     d.Get("values").(string),
+		Version:    ">0.0.0-0",
+	}
+
+	release, err := r.New()
+	if err != nil {
+		return err
+	}
+
+	if err := release.Uninstall(); err != nil {
+		return err
+	}
+
 	d.SetId("")
+
 	return nil
-}
-
-// TODO All below should be moved to the library
-type Client struct {
-	c clientcmd.ClientConfig
-}
-
-// Implemented (part of k8s.io/cli-runtime/pkg/genericclioptions.RESTClientGetter)
-func (c *Client) ToRESTMapper() (meta.RESTMapper, error) {
-	d, err := c.ToDiscoveryClient()
-	if err != nil {
-		return nil, err
-	}
-
-	mapper := restmapper.NewDeferredDiscoveryRESTMapper(d)
-	expander := restmapper.NewShortcutExpander(mapper, d)
-	return expander, nil
-}
-
-// Implemented (part of k8s.io/cli-runtime/pkg/genericclioptions.RESTClientGetter)
-func (c *Client) ToDiscoveryClient() (discovery.CachedDiscoveryInterface, error) {
-	cc, err := c.ToRESTConfig()
-	if err != nil {
-		return nil, err
-	}
-
-	d, err := discovery.NewDiscoveryClientForConfig(cc)
-	if err != nil {
-		return nil, err
-	}
-
-	return memory.NewMemCacheClient(d), nil
-}
-
-// Implemented (part of k8s.io/cli-runtime/pkg/genericclioptions.RESTClientGetter)
-func (c *Client) ToRawKubeConfigLoader() clientcmd.ClientConfig {
-	return c.c
-}
-
-// Implemented (part of k8s.io/cli-runtime/pkg/genericclioptions.RESTClientGetter)
-func (c *Client) ToRESTConfig() (*rest.Config, error) {
-	return c.c.ClientConfig()
-}
-
-func GetGetter(data []byte) (*Client, error) {
-	c, err := clientcmd.NewClientConfigFromBytes(data)
-	if err != nil {
-		return nil, err
-	}
-
-	return &Client{
-		c: c,
-	}, nil
 }
