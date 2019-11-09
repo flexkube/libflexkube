6cfb6bf632b08b41fc04bf3aca6a4b8025babf8f
Mateusz Gozdek <mgozdekof@gmail.com>
2019-11-10 00:39:07 +0100

    examples/terraform-provider-flexkube/kubelet-pool: initial commit
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

---
+++ examples/terraform-provider-flexkube/kubelet-pool/main.tf
@@ -0,0 +1,28 @@
+provider "flexkube" {}
+
+module "root_pki" {
+  source = "git::https://github.com/invidian/terraform-root-pki.git"
+
+  organization = "example"
+}
+
+module "kubernetes_pki" {
+  source = "git::https://github.com/invidian/terraform-kubernetes-pki.git"
+
+  root_ca_cert      = module.root_pki.root_ca_cert
+  root_ca_key       = module.root_pki.root_ca_key
+  root_ca_algorithm = module.root_pki.root_ca_algorithm
+
+  api_server_ips            = ["127.0.0.1"]
+  api_server_external_ips   = ["127.0.1.1"]
+  api_server_external_names = ["kube-apiserver.example.com"]
+  organization = "example"
+}
+
+resource "flexkube_kubelet_pool" "controller" {
+  config = templatefile("./config.yaml.tmpl", {
+    kubelet_addresses         = ["127.0.0.1"]
+    kubelet_pod_cidrs         = ["10.42.0.0/24"]
+    kubernetes_ca_certificate = module.kubernetes_pki.kubernetes_ca_cert
+  })
+}
