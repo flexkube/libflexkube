0a2ac7fd777c0049f492b5d67af1cb69a0fa5451
Mateusz Gozdek <mgozdekof@gmail.com>
2019-11-14 20:00:27 +0100

    pkg/container/runtime/docker/docker_test: inline test logic
    As it is a recommended styling when we only care about the error value.
    
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- pkg/container/runtime/docker/docker_test.go
+++ pkg/container/runtime/docker/docker_test.go
@@ -7,8 +7,7 @@ func TestNewClient(t *testing.T) {
 	// TODO does this kind of simple tests make sense? Integration tests calls the same thing
 	// anyway. Or maybe we should simply skip error checking in itegration tests to simplify them?
 	c := &Config{}
-	_, err := c.New()
-	if err != nil {
+	if _, err := c.New(); err != nil {
 		t.Errorf("Creating new docker client should work, got: %s", err)
 	}
 }
