316956f7988c7da5e89662829079c0ee6748f10d
Mateusz Gozdek <mgozdekof@gmail.com>
2019-12-05 09:30:26 +0100

    pkg/defaults: group all constants
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- pkg/defaults/defaults.go
+++ pkg/defaults/defaults.go
@@ -1,14 +1,16 @@
 package defaults
 
-// EtcdImage points to a default Docker image, which will be used for running etcd.
-// We use etcd 3.3.17, because 3.4.x is not yet supported
-const EtcdImage = "gcr.io/etcd-development/etcd:v3.3.17"
+const (
+	// EtcdImage points to a default Docker image, which will be used for running etcd.
+	// We use etcd 3.3.17, because 3.4.x is not yet supported
+	EtcdImage = "gcr.io/etcd-development/etcd:v3.3.17"
 
-// KubernetesImage is a default container image used for all kubernetes containers
-const KubernetesImage = "k8s.gcr.io/hyperkube:v1.16.3"
+	// KubernetesImage is a default container image used for all kubernetes containers
+	KubernetesImage = "k8s.gcr.io/hyperkube:v1.16.3"
 
-// HAProxyImage is a default container image for APILoadBalancer
-const HAProxyImage = "haproxy:2.0.7-alpine"
+	// HAProxyImage is a default container image for APILoadBalancer
+	HAProxyImage = "haproxy:2.0.7-alpine"
 
-// DockerAPIVersion is a default API version used when talking to Docker runtime
-const DockerAPIVersion = "v1.40"
+	// DockerAPIVersion is a default API version used when talking to Docker runtime
+	DockerAPIVersion = "v1.40"
+)
