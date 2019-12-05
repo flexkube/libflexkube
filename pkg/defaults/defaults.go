008ed782edc3e500c820c1654ab02f0e9939fa3e
Mateusz Gozdek <mgozdekof@gmail.com>
2019-12-05 09:33:59 +0100

    pkg/defaults/defaults: add missing dots to comments
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- pkg/defaults/defaults.go
+++ pkg/defaults/defaults.go
@@ -2,15 +2,15 @@ package defaults
 
 const (
 	// EtcdImage points to a default Docker image, which will be used for running etcd.
-	// We use etcd 3.3.17, because 3.4.x is not yet supported
+	// We use etcd 3.3.17, because 3.4.x is not yet supported.
 	EtcdImage = "gcr.io/etcd-development/etcd:v3.3.17"
 
-	// KubernetesImage is a default container image used for all kubernetes containers
+	// KubernetesImage is a default container image used for all kubernetes containers.
 	KubernetesImage = "k8s.gcr.io/hyperkube:v1.16.3"
 
-	// HAProxyImage is a default container image for APILoadBalancer
+	// HAProxyImage is a default container image for APILoadBalancer.
 	HAProxyImage = "haproxy:2.0.7-alpine"
 
-	// DockerAPIVersion is a default API version used when talking to Docker runtime
+	// DockerAPIVersion is a default API version used when talking to Docker runtime.
 	DockerAPIVersion = "v1.40"
 )
