a7c968523eca52f6cc3a4a63a23b94772a456ab3
Mateusz Gozdek <mgozdekof@gmail.com>
2019-12-05 11:37:24 +0100

    pkg/host/transport/ssh: add default constants
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

---
+++ pkg/host/transport/ssh/defaults.go
@@ -0,0 +1,12 @@
+package ssh
+
+const (
+	// User is a default user used for SSH connections.
+	User = "root"
+
+	// ConnectionTimeout is a default time SSH will wait while connecting to unreachable server.
+	ConnectionTimeout = "30s"
+
+	// Port is a default port used for SSH connections.
+	Port = 22
+)
