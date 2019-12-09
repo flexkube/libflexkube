f013635b32306f3e495ae3d1bb1d3a5c85e3d9bb
Mateusz Gozdek <mgozdekof@gmail.com>
2019-12-10 00:54:29 +0100

    pkg/defaults: update default etcd version to 3.4.3
    As we ship now k8s 1.17, which also use this version as a default.
    
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- pkg/defaults/defaults.go
+++ pkg/defaults/defaults.go
@@ -2,8 +2,7 @@ package defaults
 
 const (
 	// EtcdImage points to a default Docker image, which will be used for running etcd.
-	// We use etcd 3.3.17, because 3.4.x is not yet supported.
-	EtcdImage = "gcr.io/etcd-development/etcd:v3.3.17"
+	EtcdImage = "gcr.io/etcd-development/etcd:v3.4.3"
 
 	// KubernetesImage is a default container image used for all kubernetes containers.
 	KubernetesImage = "k8s.gcr.io/hyperkube:v1.17.0"
