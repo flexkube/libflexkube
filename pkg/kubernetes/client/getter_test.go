ddf4d925e552d74e8282e43a37fcfa5215ad5624
Mateusz Gozdek <mgozdekof@gmail.com>
2020-01-03 00:08:12 +0100

    pkg/kubernetes/client: add unit tests for getter
    Now with new helpers it is easy to actually test it.
    
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

---
+++ pkg/kubernetes/client/getter_test.go
@@ -0,0 +1,22 @@
+package client
+
+import (
+	"testing"
+)
+
+func TestGetter(t *testing.T) {
+	kubeconfig := GetKubeconfig(t)
+
+	g, err := NewGetter([]byte(kubeconfig))
+	if err != nil {
+		t.Fatalf("Creating getter should work, got: %v", err)
+	}
+
+	if _, err := g.ToDiscoveryClient(); err != nil {
+		t.Errorf("Turning getter into discovery client should work, got: %v", err)
+	}
+
+	if _, err := g.ToRESTMapper(); err != nil {
+		t.Errorf("Turning getter into REST mapper should work, got: %v", err)
+	}
+}
