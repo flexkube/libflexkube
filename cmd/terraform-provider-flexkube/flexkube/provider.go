ff32ca493733b76e6f1da3f3ae44d64216371ac7
Mateusz Gozdek <mgozdekof@gmail.com>
2020-01-05 23:20:38 +0100

    cmd/terraform-provider-flexkube: add mutex for helm client
    So it is not accessed concurrently, as it uses AddToScheme, which is not
    thread-safe and may cause the provider to crash.
    
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- cmd/terraform-provider-flexkube/flexkube/provider.go
+++ cmd/terraform-provider-flexkube/flexkube/provider.go
@@ -1,6 +1,8 @@
 package flexkube
 
 import (
+	"sync"
+
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/terraform"
 )
@@ -15,5 +17,18 @@ func Provider() terraform.ResourceProvider {
 			"flexkube_apiloadbalancer_pool": resourceAPILoadBalancerPool(),
 			"flexkube_helm_release":         resourceHelmRelease(),
 		},
+		ConfigureFunc: providerConfigure,
 	}
+}
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	return &meta{
+		helmClientLock: sync.Mutex{},
+	}, nil
+}
+
+// Meta is the meta information structure for the provider
+type meta struct {
+	// Mutex to create only one helm client as a time, as it is not thread-safe.
+	helmClientLock sync.Mutex
 }
