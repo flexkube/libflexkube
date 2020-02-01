c1fa70ee1d4c3a894b9424ba4e3d2e2ae65d51c2
Mateusz Gozdek <mgozdekof@gmail.com>
2020-02-01 01:06:25 +0100

    pkg/container/containers.go: add more unit tests
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- pkg/container/containers_test.go
+++ pkg/container/containers_test.go
@@ -109,6 +109,59 @@ func TestFilesToUpdateEmpty(t *testing.T) {
 	}
 }
 
+func TestValidateEmpty(t *testing.T) {
+	cc := &Containers{}
+
+	if err := cc.Validate(); err == nil {
+		t.Fatalf("Empty containers object shouldn't be valid")
+	}
+}
+
+func TestValidateNil(t *testing.T) {
+	var cc Containers
+
+	if err := cc.Validate(); err == nil {
+		t.Fatalf("nil containers object shouldn't be valid")
+	}
+}
+
+func TestValidateNoContainers(t *testing.T) {
+	cc := &Containers{
+		DesiredState:  ContainersState{},
+		PreviousState: ContainersState{},
+	}
+
+	if err := cc.Validate(); err == nil {
+		t.Fatalf("Containers object without any containers shouldn't be valid")
+	}
+}
+
+func TestValidateBadDesiredContainers(t *testing.T) {
+	cc := &Containers{
+		DesiredState: ContainersState{
+			"foo": &HostConfiguredContainer{},
+		},
+		PreviousState: ContainersState{},
+	}
+
+	if err := cc.Validate(); err == nil {
+		t.Fatalf("Containers object with bad desired container shouldn't be valid")
+	}
+}
+
+func TestValidateBadCurrentContainers(t *testing.T) {
+	cc := &Containers{
+		DesiredState: ContainersState{},
+		PreviousState: ContainersState{
+			"foo": &HostConfiguredContainer{},
+		},
+	}
+
+	if err := cc.Validate(); err == nil {
+		t.Fatalf("Containers object with bad current container shouldn't be valid")
+	}
+}
+
 func TestIsUpdatableWithoutCurrentState(t *testing.T) {
 	c := &containers{
 		desiredState: containersState{
@@ -277,5 +330,122 @@ func TestDiffContainer(t *testing.T) {
 
 	if diff == "" {
 		t.Fatalf("Container with config updates should return diff")
+	}
+}
+
+func TestEnsureRunningNonExistent(t *testing.T) {
+	c := &containers{
+		currentState: containersState{},
+	}
+
+	if err := ensureRunning(c.currentState["bar"]); err == nil {
+		t.Fatalf("Ensuring that non existing container is running should fail")
+	}
+}
+
+func TestEnsureRunning(t *testing.T) {
+	c := &containers{
+		desiredState: containersState{
+			"foo": &hostConfiguredContainer{
+				container: Container{
+					Config: types.ContainerConfig{},
+				},
+			},
+		},
+		currentState: containersState{
+			"foo": &hostConfiguredContainer{
+				container: Container{
+					Config: types.ContainerConfig{},
+					Status: &types.ContainerStatus{
+						Status: "running",
+					},
+				},
+			},
+		},
+	}
+
+	if err := ensureRunning(c.currentState["foo"]); err != nil {
+		t.Fatalf("Ensuring that running container is running should succeed, got: %v", err)
+	}
+}
+
+func TestEnsureExists(t *testing.T) {
+	c := &containers{
+		currentState: containersState{
+			"foo": &hostConfiguredContainer{
+				container: Container{
+					Config: types.ContainerConfig{},
+					Status: &types.ContainerStatus{
+						Status: "running",
+					},
+				},
+			},
+		},
+	}
+
+	if err := c.ensureExists("foo"); err != nil {
+		t.Fatalf("Ensuring that existing container exists should succeed, got: %v", err)
+	}
+}
+
+func TestEnsureHostNoDiff(t *testing.T) {
+	c := &containers{
+		desiredState: containersState{
+			"foo": &hostConfiguredContainer{
+				host: host.Host{
+					DirectConfig: &direct.Config{},
+				},
+			},
+		},
+		currentState: containersState{
+			"foo": &hostConfiguredContainer{
+				host: host.Host{
+					DirectConfig: &direct.Config{},
+				},
+			},
+		},
+	}
+
+	if err := c.ensureHost("foo"); err != nil {
+		t.Fatalf("Ensuring that container's host configuration is up to date should succeed, got: %v", err)
+	}
+}
+
+func TestEnsureContainerNoDiff(t *testing.T) {
+	c := &containers{
+		desiredState: containersState{
+			"foo": &hostConfiguredContainer{
+				container: Container{
+					Config: types.ContainerConfig{},
+				},
+			},
+		},
+		currentState: containersState{
+			"foo": &hostConfiguredContainer{
+				container: Container{
+					Config: types.ContainerConfig{},
+				},
+			},
+		},
+	}
+
+	if err := c.ensureContainer("foo"); err != nil {
+		t.Fatalf("Ensuring that container configuration is up to date should succeed, got: %v", err)
+	}
+}
+
+// recreate()
+func TestRecreateNonExistent(t *testing.T) {
+	c := &containers{}
+	if err := c.recreate("foo"); err == nil {
+		t.Fatalf("Recreating on empty containers should fail")
+	}
+}
+
+// Execute()
+func TestExecuteNoCurrentState(t *testing.T) {
+	c := &containers{}
+	if err := c.Execute(); err == nil {
+		t.Fatalf("Execute without current state should fail")
 	}
 }
