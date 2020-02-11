6068e6a17c2c7ff939049d03b878570327a80742
Mateusz Gozdek <mgozdekof@gmail.com>
2020-02-12 00:24:54 +0100

    pkg/defaults: update default HAProxy version to 2.1.2
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- pkg/defaults/defaults.go
+++ pkg/defaults/defaults.go
@@ -8,7 +8,7 @@ const (
 	KubernetesImage = "k8s.gcr.io/hyperkube:v1.17.3"
 
 	// HAProxyImage is a default container image for APILoadBalancer.
-	HAProxyImage = "haproxy:2.1.0-alpine"
+	HAProxyImage = "haproxy:2.1.2-alpine"
 
 	// DockerAPIVersion is a default API version used when talking to Docker runtime.
 	DockerAPIVersion = "v1.38"
