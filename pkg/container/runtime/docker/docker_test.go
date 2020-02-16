58463a6be80bb3e2a144a8b7299cba6ede1526b6
Mateusz Gozdek <mgozdekof@gmail.com>
2020-02-16 13:57:36 +0100

    pkg/container/runtime/docker/docker_test.go: add tests for Status()
    Using new fake client.
    
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- pkg/container/runtime/docker/docker_test.go
+++ pkg/container/runtime/docker/docker_test.go
@@ -1,6 +1,13 @@
 package docker
 
-import "testing"
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	dockertypes "github.com/docker/docker/api/types"
+	"github.com/docker/docker/errdefs"
+)
 
 // New()
 func TestNewClient(t *testing.T) {
@@ -42,5 +49,73 @@ func TestSanitizeImageNameWithTag(t *testing.T) {
 
 	if g := sanitizeImageName(e); g != e {
 		t.Fatalf("Expected '%s', got '%s'", e, g)
+	}
+}
+
+// Status()
+func TestStatus(t *testing.T) {
+	es := "running"
+
+	d := &docker{
+		ctx: context.Background(),
+		cli: &FakeClient{
+			ContainerInspectF: func(ctx context.Context, id string) (dockertypes.ContainerJSON, error) {
+				return dockertypes.ContainerJSON{
+					ContainerJSONBase: &dockertypes.ContainerJSONBase{
+						State: &dockertypes.ContainerState{
+							Status: es,
+						},
+					},
+				}, nil
+			},
+		},
+	}
+
+	s, err := d.Status("foo")
+	if err != nil {
+		t.Fatalf("Checking for status should succeed, got: %v", err)
+	}
+
+	if s.ID == "" {
+		t.Fatalf("ID in status of existing container should not be empty")
+	}
+
+	if s.Status != es {
+		t.Fatalf("Received status should be %s, got %s", es, s.Status)
+	}
+}
+
+func TestStatusNotFound(t *testing.T) {
+	d := &docker{
+		ctx: context.Background(),
+		cli: &FakeClient{
+			ContainerInspectF: func(ctx context.Context, id string) (dockertypes.ContainerJSON, error) {
+				return dockertypes.ContainerJSON{}, errdefs.NotFound(fmt.Errorf("not found"))
+			},
+		},
+	}
+
+	s, err := d.Status("foo")
+	if err != nil {
+		t.Fatalf("Checking for status should succeed, got: %v", err)
+	}
+
+	if s.ID != "" {
+		t.Fatalf("ID in status of non-existing container should be empty")
+	}
+}
+
+func TestStatusRuntimeError(t *testing.T) {
+	d := &docker{
+		ctx: context.Background(),
+		cli: &FakeClient{
+			ContainerInspectF: func(ctx context.Context, id string) (dockertypes.ContainerJSON, error) {
+				return dockertypes.ContainerJSON{}, fmt.Errorf("can't check status of container")
+			},
+		},
+	}
+
+	if _, err := d.Status("foo"); err == nil {
+		t.Fatalf("Checking for status should fail")
 	}
 }
