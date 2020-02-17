67150dd2860a86569242e007ea1138b8d463b549
Mateusz Gozdek <mgozdekof@gmail.com>
2020-02-17 01:59:43 +0100

    pkg/container/hostconfiguredcontainer_test.go: add more tests
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- pkg/container/hostconfiguredcontainer_test.go
+++ pkg/container/hostconfiguredcontainer_test.go
@@ -5,6 +5,8 @@ import (
 	"net"
 	"testing"
 
+	"github.com/flexkube/libflexkube/pkg/container/runtime"
+	"github.com/flexkube/libflexkube/pkg/container/types"
 	"github.com/flexkube/libflexkube/pkg/host"
 	"github.com/flexkube/libflexkube/pkg/host/transport/direct"
 )
@@ -91,5 +93,119 @@ func TestConnectAndForward(t *testing.T) {
 
 	if s == "" {
 		t.Fatalf("Returned forwarded address shouldn't be empty")
+	}
+}
+
+// Status()
+func TestHostConfiguredContainerStatusNotExist(t *testing.T) {
+	h := &hostConfiguredContainer{
+		container: &container{},
+	}
+
+	if err := h.Status(); err != nil {
+		t.Fatalf("checking status of non existing container should succeed, got: %v", err)
+	}
+}
+
+func TestHostConfiguredContainerStatus(t *testing.T) {
+	h := &hostConfiguredContainer{
+		host: host.Host{
+			DirectConfig: &direct.Config{},
+		},
+		container: &container{
+			base: base{
+				runtimeConfig: &runtime.FakeConfig{
+					Runtime: &runtime.Fake{
+						StatusF: func(id string) (types.ContainerStatus, error) {
+							return types.ContainerStatus{}, nil
+						},
+					},
+				},
+			},
+			status: types.ContainerStatus{
+				ID: "foo",
+			},
+		},
+	}
+
+	if err := h.Status(); err != nil {
+		t.Fatalf("checking status of existing container should succeed, got: %v", err)
+	}
+}
+
+// createConfigurationContainer()
+func TestHostConfiguredContainerCreateConfigurationContainer(t *testing.T) {
+	h := &hostConfiguredContainer{
+		container: &container{
+			base: base{
+				runtime: &runtime.Fake{
+					CreateF: func(config *types.ContainerConfig) (string, error) {
+						return "", fmt.Errorf("creating failed")
+					},
+				},
+			},
+		},
+	}
+
+	if err := h.createConfigurationContainer(); err == nil {
+		t.Fatalf("creating configuration container should fail")
+	}
+}
+
+// removeConfigurationContainer()
+func TestHostConfiguredContainerRemoveConfigurationContainer(t *testing.T) {
+	deleted := false
+	i := "foo"
+
+	h := &hostConfiguredContainer{
+		configContainer: &containerInstance{
+			status: types.ContainerStatus{
+				ID: i,
+			},
+			base: base{
+				runtime: &runtime.Fake{
+					StatusF: func(id string) (types.ContainerStatus, error) {
+						return types.ContainerStatus{
+							ID: i,
+						}, nil
+					},
+					DeleteF: func(id string) error {
+						if id != i {
+							t.Fatalf("should remove container %s, got %s", i, id)
+						}
+
+						deleted = true
+
+						return nil
+					},
+				},
+			},
+		},
+	}
+
+	if err := h.removeConfigurationContainer(); err != nil {
+		t.Fatalf("removing configuration container should succeed, got: %v", err)
+	}
+
+	if !deleted {
+		t.Fatalf("removing existing configuration container should call Delete()")
+	}
+}
+
+func TestHostConfiguredContainerRemoveConfigurationContainerFailStatus(t *testing.T) {
+	h := &hostConfiguredContainer{
+		configContainer: &containerInstance{
+			base: base{
+				runtime: &runtime.Fake{
+					StatusF: func(id string) (types.ContainerStatus, error) {
+						return types.ContainerStatus{}, fmt.Errorf("checking status failed")
+					},
+				},
+			},
+		},
+	}
+
+	if err := h.removeConfigurationContainer(); err == nil {
+		t.Fatalf("removing configuration container should fail")
 	}
 }
