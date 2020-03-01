7d080d37d6b6373f75fe44bd5ea125b0fee1e47a
Mateusz Gozdek <mgozdekof@gmail.com>
2020-03-02 00:47:34 +0100

    cmd/container-runner: use cli/containerrunner
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- cmd/container-runner/main.go
+++ cmd/container-runner/main.go
@@ -1,72 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"os"
 
-	"github.com/flexkube/libflexkube/pkg/container"
+	"github.com/flexkube/libflexkube/cli/containerrunner"
 )
 
-func readYamlFile(file string) ([]byte, error) {
-	if _, err := os.Stat(file); os.IsNotExist(err) {
-		return []byte(""), nil
-	}
-
-	c, err := ioutil.ReadFile(file)
-	if err != nil {
-		return nil, err
-	}
-	// Workaround for empty YAML file
-	if string(c) == "{}\n" {
-		return []byte{}, nil
-	}
-
-	return c, nil
-}
-
 func main() {
-	s, err := readYamlFile("state.yaml")
-	if err != nil {
-		panic(err)
-	}
-
-	config, err := readYamlFile("config.yaml")
-	if err != nil {
-		panic(err)
-	}
-
-	c, err := container.FromYaml([]byte(string(s) + string(config)))
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Checking current state...\n\n")
-
-	if err := c.CheckCurrentState(); err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Applying changes...\n\n")
-
-	if err := c.Execute(); err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("\nDumping current state to file...\n")
-
-	state, err := c.CurrentStateToYaml()
-	if err != nil {
-		panic(err)
-	}
-
-	if string(state) == "{}\n" {
-		state = []byte{}
-	}
-
-	if err := ioutil.WriteFile("state.yaml", state, 0644); err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Done")
+	os.Exit(containerrunner.Run())
 }
