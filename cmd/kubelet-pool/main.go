7f93705f4aa68528ca2ad654f00f824548226ee4
Mateusz Gozdek <mgozdekof@gmail.com>
2020-03-02 00:54:52 +0100

    cmd/kubelet-pool: use cli/kubeletpool
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- cmd/kubelet-pool/main.go
+++ cmd/kubelet-pool/main.go
@@ -1,79 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"os"
 
-	"github.com/flexkube/libflexkube/pkg/kubelet"
+	"github.com/flexkube/libflexkube/cli/kubeletpool"
 )
 
-func readYamlFile(file string) ([]byte, error) {
-	if _, err := os.Stat(file); os.IsNotExist(err) {
-		return []byte(""), nil
-	}
-
-	c, err := ioutil.ReadFile(file)
-	if err != nil {
-		return nil, err
-	}
-
-	// Workaround for empty YAML file
-	if string(c) == "{}\n" {
-		return []byte{}, nil
-	}
-
-	return c, nil
-}
-
 func main() {
-	fmt.Println("Reading state file state.yaml")
-
-	s, err := readYamlFile("state.yaml")
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Reading config file config.yaml")
-
-	config, err := readYamlFile("config.yaml")
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Creating kubelet pool object")
-
-	c, err := kubelet.FromYaml([]byte(string(s) + string(config)))
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Checking current state of the pool")
-
-	if err := c.CheckCurrentState(); err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Deploying pool updates")
-
-	if err := c.Deploy(); err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Saving new pool state to state.yaml file")
-
-	state, err := c.StateToYaml()
-	if err != nil {
-		panic(err)
-	}
-
-	if string(state) == "{}\n" {
-		state = []byte{}
-	}
-
-	if err := ioutil.WriteFile("state.yaml", state, 0644); err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Run complete")
+	os.Exit(kubeletpool.Run())
 }
