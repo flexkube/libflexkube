a966997f25a6bc80d565658d5786ecf9fe4562cf
Mateusz Gozdek <mgozdekof@gmail.com>
2020-04-18 16:48:51 +0200

    pkg/apiloadbalancer: use HTTP probes for the backend
    So it does not cause the TLS handshake errors.
    
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- pkg/apiloadbalancer/api-loadbalancer.go
+++ pkg/apiloadbalancer/api-loadbalancer.go
@@ -54,8 +54,9 @@ frontend kube-apiserver
   default_backend kube-apiserver
 
 backend kube-apiserver
+  option httpchk GET /healthz HTTP/1.1\r\nHost:\ kube-apiserver
   {{- range $i, $s := .Servers }}
-  server {{ $i }} {{ $s }} check
+  server {{ $i }} {{ $s }} verify none check check-ssl
   {{- end }}
 `
 
