c9b4c024bc19bbcf600bc577457e56a812697310
Mateusz Gozdek <mgozdekof@gmail.com>
2020-05-24 22:18:42 +0200

    cli/flexkube: bump Version to v0.3.0
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- cli/flexkube/cli.go
+++ cli/flexkube/cli.go
@@ -8,7 +8,7 @@ import (
 
 const (
 	// Version is a version printed by the --version flag.
-	Version = "v0.3.0-unreleased"
+	Version = "v0.3.0"
 )
 
 // Run executes flexkube CLI binary with given arguments (usually os.Args).
