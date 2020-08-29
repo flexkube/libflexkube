e63e6b84c870fc3066aa3ad08da32c1c7cf365d6
Mateusz Gozdek <mgozdekof@gmail.com>
2020-08-29 11:45:13 +0200

    cli/flexkube: bump Version to v0.4.0-unreleased
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- cli/flexkube/cli.go
+++ cli/flexkube/cli.go
@@ -8,7 +8,7 @@ import (
 
 const (
 	// Version is a version printed by the --version flag.
-	Version = "v0.3.3"
+	Version = "v0.4.0-unreleased"
 
 	// YesFlag is a const for --yes flag.
 	YesFlag = "yes"
