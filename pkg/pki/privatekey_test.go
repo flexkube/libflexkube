1a24f65a3f584428ef6d33b61160247caf2c7f11
Mateusz Gozdek <mgozdekof@gmail.com>
2020-09-19 18:02:45 +0200

    pkg/pki: add method to validate private keys
    Given that PKI package is mainly responsible for managing certificates,
    it sounds like a good place to put this function, as resources which
    supports PKI integration import it anyway.
    
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

---
+++ pkg/pki/privatekey_test.go
@@ -0,0 +1,56 @@
+package pki_test
+
+import (
+	"testing"
+
+	"github.com/flexkube/libflexkube/internal/utiltest"
+	"github.com/flexkube/libflexkube/pkg/pki"
+)
+
+func TestPrivateKeyParse(t *testing.T) {
+	cases := map[string]struct {
+		key string
+		err bool
+	}{
+		"bad": {
+			"doh",
+			true,
+		},
+		"badpem": {
+			`
+-----BEGIN CERTIFICATE-----
+aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1kUXc0dzlXZ1hjUQo=
+-----END CERTIFICATE-----
+`,
+			true,
+		},
+		"pkcs1": {
+			utiltest.GeneratePKCS1PrivateKey(t),
+			false,
+		},
+		"ec": {
+			utiltest.GenerateECPrivateKey(t),
+			false,
+		},
+		"rsa": {
+			utiltest.GenerateRSAPrivateKey(t),
+			false,
+		},
+	}
+
+	for n, c := range cases {
+		c := c
+
+		t.Run(n, func(t *testing.T) {
+			err := pki.ValidatePrivateKey(c.key)
+
+			if c.err && err == nil {
+				t.Fatalf("Expected error and didn't get any.")
+			}
+
+			if !c.err && err != nil {
+				t.Fatalf("Didn't expect error, got: %v", err)
+			}
+		})
+	}
+}
