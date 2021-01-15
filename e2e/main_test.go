f5d55b9471f40de34592fe4d29d9fd51cd407cb9
Mateusz Gozdek <mgozdekof@gmail.com>
2021-01-15 10:50:11 +0100

    e2e: update charts to latest versions
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- e2e/main_test.go
+++ e2e/main_test.go
@@ -94,15 +94,15 @@ func defaultE2EConfig(t *testing.T) e2eConfig {
 		Charts: charts{
 			KubeAPIServer: chart{
 				Source:  "flexkube/kube-apiserver",
-				Version: "0.3.1",
+				Version: "0.3.2",
 			},
 			Kubernetes: chart{
 				Source:  "flexkube/kubernetes",
-				Version: "0.4.1",
+				Version: "0.4.2",
 			},
 			KubeProxy: chart{
 				Source:  "flexkube/kube-proxy",
-				Version: "0.3.1",
+				Version: "0.3.2",
 			},
 			TLSBootstrapping: chart{
 				Source:  "flexkube/tls-bootstrapping",
