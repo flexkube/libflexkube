59c58fc22843de9af35af28fbf2b62e70bb152f6
Mateusz Gozdek <mgozdekof@gmail.com>
2021-03-02 21:52:15 +0100

    pkg/kubernetes/client: expose wait parameters for PingWait()
    So they can be changed while testing and by users.
    
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- pkg/kubernetes/client/client_test.go
+++ pkg/kubernetes/client/client_test.go
@@ -1,7 +1,11 @@
 package client
 
 import (
+	"errors"
 	"testing"
+	"time"
+
+	"k8s.io/apimachinery/pkg/util/wait"
 )
 
 func TestCheckNodeExistsFakeKubeconfig(t *testing.T) {
@@ -59,8 +63,8 @@ func TestLabelNodeFakeKubeconfig(t *testing.T) {
 	}
 }
 
-// Ping() tests.
-func TestPingFakeKubeconfig(t *testing.T) {
+// PingWait() tests.
+func TestPingWaitFakeKubeconfig(t *testing.T) {
 	t.Parallel()
 
 	kubeconfig := GetKubeconfig(t)
@@ -70,14 +74,8 @@ func TestPingFakeKubeconfig(t *testing.T) {
 		t.Fatalf("Failed creating client: %v", err)
 	}
 
-	e, err := c.(*client).Ping()
-
-	if e == true {
-		t.Errorf("Ping should never return true with fake kubeconfig")
-	}
-
-	if err != nil {
-		t.Errorf("Ping should swallow all errors and just return boolean value")
+	if err := c.PingWait(1*time.Second, 1*time.Second); !errors.Is(err, wait.ErrWaitTimeout) {
+		t.Fatalf("Ping with fake config should always timeout, got: %v", err)
 	}
 }
 
