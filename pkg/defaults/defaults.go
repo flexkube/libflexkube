f95e130eaefeb1ece0ae523f89c085d6af278434
Mateusz Gozdek <mgozdekof@gmail.com>
2021-05-12 11:29:54 +0200

    pkg/defaults: update etcd to v3.4.16
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- pkg/defaults/defaults.go
+++ pkg/defaults/defaults.go
@@ -3,7 +3,7 @@ package defaults
 
 const (
 	// EtcdImage points to a default Docker image, which will be used for running etcd.
-	EtcdImage = "quay.io/coreos/etcd:v3.4.15"
+	EtcdImage = "quay.io/coreos/etcd:v3.4.16"
 
 	// KubeAPIServerImage points to a default Docker image, which will be used for
 	// running kube-apiserver.
