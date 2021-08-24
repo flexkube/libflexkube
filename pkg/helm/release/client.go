29e2cb24a7543005f2fa57f588d7e030cc4bf1db
Mateusz Gozdek <mgozdekof@gmail.com>
2021-08-24 13:36:31 +0200

    pkg/helm/release: don't explicitly add apiextensions/v1beta1 to scheme
    This should no longer be needed, as apiextensions/v1beta1 is now removed
    from Kubernetes 1.22 and long time deprecated.
    
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- pkg/helm/release/client.go
+++ pkg/helm/release/client.go
@@ -4,9 +4,7 @@ import (
 	"fmt"
 
 	"helm.sh/helm/v3/pkg/kube"
-	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/kubectl/pkg/cmd/util"
 
 	"github.com/flexkube/libflexkube/pkg/kubernetes/client"
@@ -14,12 +12,6 @@ import (
 
 // TODO maybe we should return struct here?
 func newClients(kubeconfig string) (*client.Getter, *kube.Client, *kubernetes.Clientset, error) {
-	// Inlining helm.sh/helm/v3/pkg/kube.New() to be able to override the config
-	if err := v1beta1.AddToScheme(scheme.Scheme); err != nil {
-		// According to helm, this error should never happen
-		return nil, nil, nil, fmt.Errorf("unknown error occurred: %w", err)
-	}
-
 	g, err := client.NewGetter([]byte(kubeconfig))
 	if err != nil {
 		return nil, nil, nil, fmt.Errorf("creating Kubernetes client getter: %w", err)
