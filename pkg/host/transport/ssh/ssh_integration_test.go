799bed72d25efcb4eccce87000ff61199846ac46
Mateusz Gozdek <mgozdekof@gmail.com>
2022-01-27 00:42:49 +0100

    pkg: make integration tests files configurable via env variables
    This way, we can run integration tests both with Vagrant and Docker.
    
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- pkg/host/transport/ssh/ssh_integration_test.go
+++ pkg/host/transport/ssh/ssh_integration_test.go
@@ -11,6 +11,7 @@ import (
 	"os"
 	"path/filepath"
 	"reflect"
+	"strconv"
 	"strings"
 	"testing"
 
@@ -22,7 +23,11 @@ import (
 func TestPasswordAuth(t *testing.T) {
 	unsetSSHAuthSockEnv(t)
 
-	passwordFilePath := "/home/core/.ssh/password"
+	passwordFilePath := os.Getenv("TEST_INTEGRATION_SSH_PASSWORD_FILE")
+
+	if passwordFilePath == "" {
+		passwordFilePath = "/home/core/.ssh/password"
+	}
 
 	pass, err := ioutil.ReadFile(passwordFilePath)
 	if err != nil {
@@ -35,7 +40,7 @@ func TestPasswordAuth(t *testing.T) {
 		ConnectionTimeout: "5s",
 		RetryTimeout:      "5s",
 		RetryInterval:     "1s",
-		Port:              Port,
+		Port:              testPort(t),
 		Password:          strings.TrimSpace(string(pass)),
 	}
 
@@ -60,7 +65,7 @@ func TestPasswordAuthFail(t *testing.T) {
 		ConnectionTimeout: "5s",
 		RetryTimeout:      "5s",
 		RetryInterval:     "1s",
-		Port:              Port,
+		Port:              testPort(t),
 		Password:          "badpassword",
 	}
 
@@ -89,7 +94,11 @@ func withPrivateKey(t *testing.T) transport.Interface {
 
 	unsetSSHAuthSockEnv(t)
 
-	sshPrivateKeyPath := "/home/core/.ssh/id_rsa"
+	sshPrivateKeyPath := os.Getenv("TEST_INTEGRATION_SSH_PRIVATE_KEY_PATH")
+
+	if sshPrivateKeyPath == "" {
+		sshPrivateKeyPath = "/home/core/.ssh/id_rsa"
+	}
 
 	key, err := ioutil.ReadFile(sshPrivateKeyPath)
 	if err != nil {
@@ -102,7 +111,7 @@ func withPrivateKey(t *testing.T) transport.Interface {
 		ConnectionTimeout: "5s",
 		RetryTimeout:      "5s",
 		RetryInterval:     "1s",
-		Port:              Port,
+		Port:              testPort(t),
 		PrivateKey:        string(key),
 	}
 
@@ -239,4 +248,20 @@ func runServer(t *testing.T, socket string, expectedRequest, response []byte) {
 		fmt.Printf("Failed closing connection: %v\n", err)
 		t.Fail()
 	}
+}
+
+func testPort(t *testing.T) int {
+	t.Helper()
+
+	envPort := os.Getenv("TEST_INTEGRATION_SSH_PORT")
+	if envPort == "" {
+		return Port
+	}
+
+	port, err := strconv.Atoi(envPort)
+	if err != nil {
+		t.Fatalf("Failed parsing test port %q: %v", envPort, err)
+	}
+
+	return port
 }
