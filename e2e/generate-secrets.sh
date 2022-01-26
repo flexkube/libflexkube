cd73f4622094145743867d7b7201679a57b0b0c8
Mateusz Gozdek <mgozdekof@gmail.com>
2022-01-27 00:49:33 +0100

    e2e: add required files for running integration tests using Docker
    This includes, allow logging in as UID 0 with SSH, so we have access to
    both host test socket and Docker socket over SSH and share /tmp with
    container, so we can access test socket.
    
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- e2e/generate-secrets.sh
+++ e2e/generate-secrets.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 test -f /root/.ssh/id_rsa || ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ""
+test -f /root/.ssh/password || tr -cd "[:alnum:]" < /dev/urandom | fold -w30 | head -n1 | tr -d '\n' > /root/.ssh/password
