21c659e7da35767de9c64798e76b18ad1fc4e212
Mateusz Gozdek <mgozdekof@gmail.com>
2022-06-17 20:04:54 +0200

    pkg/defaults: update default HAProxy image to latest version v2.6.0
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- pkg/defaults/defaults.go
+++ pkg/defaults/defaults.go
@@ -22,7 +22,7 @@ const (
 	KubeletImage = "quay.io/flexkube/kubelet:v1.24.1"
 
 	// HAProxyImage is a default container image for APILoadBalancer.
-	HAProxyImage = "haproxy:2.5.7-alpine"
+	HAProxyImage = "haproxy:2.6.0-alpine"
 
 	// DockerAPIVersion is a default API version used when talking to Docker runtime.
 	DockerAPIVersion = "v1.38"
