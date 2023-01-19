662fe5e3daf4f9e9f07275dfff5be679e23f8a8c
Mateusz Gozdek <mgozdekof@gmail.com>
2023-01-19 15:42:05 +0100

    Update Kubernetes to v1.26.1
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- pkg/defaults/defaults.go
+++ pkg/defaults/defaults.go
@@ -7,19 +7,19 @@ const (
 
 	// KubeAPIServerImage points to a default Docker image, which will be used for
 	// running kube-apiserver.
-	KubeAPIServerImage = "registry.k8s.io/kube-apiserver:v1.26.0"
+	KubeAPIServerImage = "registry.k8s.io/kube-apiserver:v1.26.1"
 
 	// KubeControllerManagerImage points to a default Docker image, which will be used for
 	// running kube-apiserver.
-	KubeControllerManagerImage = "registry.k8s.io/kube-controller-manager:v1.26.0"
+	KubeControllerManagerImage = "registry.k8s.io/kube-controller-manager:v1.26.1"
 
 	// KubeSchedulerImage points to a default Docker image, which will be used for
 	// running kube-apiserver.
-	KubeSchedulerImage = "registry.k8s.io/kube-scheduler:v1.26.0"
+	KubeSchedulerImage = "registry.k8s.io/kube-scheduler:v1.26.1"
 
 	// KubeletImage points to a default Docker image, which will be used for
 	// running kube-apiserver.
-	KubeletImage = "quay.io/flexkube/kubelet:v1.26.0"
+	KubeletImage = "quay.io/flexkube/kubelet:v1.26.1"
 
 	// HAProxyImage is a default container image for APILoadBalancer.
 	HAProxyImage = "haproxy:2.7.1-alpine"
