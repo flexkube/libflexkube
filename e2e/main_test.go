718a1599076a456a7564451a143ab473d18669c1
Mateusz Gozdek <mgozdekof@gmail.com>
2023-07-20 17:57:55 +0200

    Update to Kubernetes v1.27.4
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- e2e/main_test.go
+++ e2e/main_test.go
@@ -99,15 +99,15 @@ func defaultE2EConfig(t *testing.T) e2eConfig {
 		Charts: charts{
 			KubeAPIServer: chart{
 				Source:  "flexkube/kube-apiserver",
-				Version: "0.7.3",
+				Version: "0.7.4",
 			},
 			Kubernetes: chart{
 				Source:  "flexkube/kubernetes",
-				Version: "0.8.3",
+				Version: "0.8.4",
 			},
 			KubeProxy: chart{
 				Source:  "flexkube/kube-proxy",
-				Version: "0.7.3",
+				Version: "0.7.4",
 			},
 			TLSBootstrapping: chart{
 				Source:  "flexkube/tls-bootstrapping",
