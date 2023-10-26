82b36b3f4caa118904aa4111b981036a286cf3b1
Mateusz Gozdek <mgozdekof@gmail.com>
2023-10-26 11:06:28 +0200

    Update to Kubernetes v1.28.3
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- e2e/main_test.go
+++ e2e/main_test.go
@@ -99,15 +99,15 @@ func defaultE2EConfig(t *testing.T) e2eConfig {
 		Charts: charts{
 			KubeAPIServer: chart{
 				Source:  "flexkube/kube-apiserver",
-				Version: "0.8.0",
+				Version: "0.8.1",
 			},
 			Kubernetes: chart{
 				Source:  "flexkube/kubernetes",
-				Version: "0.9.0",
+				Version: "0.9.1",
 			},
 			KubeProxy: chart{
 				Source:  "flexkube/kube-proxy",
-				Version: "0.8.0",
+				Version: "0.8.1",
 			},
 			TLSBootstrapping: chart{
 				Source:  "flexkube/tls-bootstrapping",
